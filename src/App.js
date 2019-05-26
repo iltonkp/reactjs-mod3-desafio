f20f85ed552177e0df97c07c084638b567d382ac
iltonkp <iltonk.si@gmail.com>
2019-05-26 14:28:20 -0300

    GoReact - mod3 - desafio - Iniciando a Página Main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const App = () => <h1>Página Inicial</h1>;
+import Main from './pages/Main';
+
+const App = () => <Main />;
 
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  state = {};
+
+  render() {
+    return <h1>Página Main</h1>;
+  }
+}
+
+export default Main;
