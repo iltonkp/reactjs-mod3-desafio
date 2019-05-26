1173095bf81d4d099af8fbdbf6f549326b4e3410
iltonkp <iltonk.si@gmail.com>
2019-05-26 15:30:32 -0300

    GoReact - mod3 - desafio - Configurando .env, global-styles e react-map-gl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
-const App = () => <Routes />;
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Routes />
+  </Fragment>
+);
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
+import MapGl from 'react-map-gl';
+
 class Main extends Component {
-  state = {};
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -8.027993,
+      longitude: -34.875826,
+      zoom: 14,
+    },
+  };
 
   render() {
-    return <h1>Página Main</h1>;
+    const { viewport } = this.state;
+
+    return (
+      <MapGl
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        onViewportChange={vPort => this.setState({ viewport: vPort })}
+        mapboxApiAccessToken="pk.eyJ1IjoiaWx0b25rcCIsImEiOiJjanc1OGZyd3ExbjJmNDBwc3gxYzdrNDZvIn0.Vo1lSJqSOWzyi8KUMeKFeg"
+      />
+    );
   }
 }
 
