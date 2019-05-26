f40b4b30c00d64d2eb12664848a48a184447e3f6
iltonkp <iltonk.si@gmail.com>
2019-05-26 16:16:00 -0300

    GoReact - mod3 - desafio - Iniciando o compoente sidebar

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from './style';
+
+const Sidebar = () => (
+  <Container>
+    {' '}
+    <ul>
+      <li>Teste 1</li>
+      <li>Teste 1</li>
+      <li>Teste 1</li>
+      <li>Teste 1</li>
+      <li>Teste 1</li>
+    </ul>
+  </Container>
+);
+
+export default Sidebar;
---
+++ src/components/sidebar/style.js
@@ -0,0 +1,17 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.aside`
+  background: #fff;
+    width: 200px;
+    display: flex;
+    height: 150px;
+    flex: 1;
+    flex-direction: column;
+    margin-top: -67%;
+    height: 96%;
+    margin-left: 15px;
+    position: absolute;
+    z-index: 3;
+    border: 1px solid #d9d9d9;
+    border-radius: 5px;
+`;
